2b75310b1641cb939db702986ee9570353f441cd
wousq <sofiakolokolnikova@gmail.com>
2025-12-29 21:44:15 +0000

    Add weights

---
+++ bm_implementation/weights.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+return_matrix = pd.read_csv(
+    "bm_implementation/return_matrix.csv",
+    index_col=0,
+    parse_dates=True
+)
+
+def equal_weights(assets):
+    n = len(assets)
+    return pd.Series(1 / n, index=assets)
+
+weights = equal_weights(return_matrix.columns)
+
+print(weights)
+print("Sum of weights:", weights.sum())
+
